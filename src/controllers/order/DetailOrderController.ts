c7a79c85e01ebdda93b33da593ed4adbf879d909
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-28 19:58:36 -0300

    fix: corrige erro de ortografia

--- src/controllers/order/DetailOrderController.ts
+++ src/controllers/order/DetailOrderController.ts
@@ -3,7 +3,7 @@ import { DetailOrderService } from "../../services/order/DetailOrderService";
 
 class DetailOrderController {
     async handle(req: Request, res: Response) {
-        const order_id = req.query.order_ir as string
+        const order_id = req.query.order_id as string
 
         const detailOrderService = new DetailOrderService();
 
