92ec2094ae7bcd9a1a480e916d19a51b4ac027a4
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-29 18:56:18 -0300

    fix: resolve bug de tmp

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,20 +1,42 @@
+// import crypto from 'crypto'
+// import multer from 'multer'
+
+// import { extname, resolve } from 'path'
+
+// export default {
+//     upload(folder: string) {
+//         return {
+//             storage: multer.diskStorage({
+//                 destination: resolve(__dirname, '..', '..', folder),
+//                 filename: (request, file, callback) => {
+//                     const fileHash = crypto.randomBytes(16).toString('hex');
+//                     const filename = `${fileHash}-${file.originalname}`
+
+//                     return callback(null, filename)
+//                 }
+//             })
+//         }
+//     }
+// }
+
 import crypto from 'crypto'
 import multer from 'multer'
-
 import { extname, resolve } from 'path'
 
 export default {
-    upload(folder: string) {
-        return {
-            storage: multer.diskStorage({
-                destination: resolve(__dirname, '..', '..', folder),
-                filename: (request, file, callback) => {
-                    const fileHash = crypto.randomBytes(16).toString('hex');
-                    const filename = `${fileHash}-${file.originalname}`
+  upload(folder: string) {
+    // Se for rodar em ambiente read-only, força o uso de /tmp
+    const destinationPath = folder === 'tmp' ? '/tmp' : resolve(__dirname, '..', '..', folder)
 
-                    return callback(null, filename)
-                }
-            })
+    return {
+      storage: multer.diskStorage({
+        destination: destinationPath,
+        filename: (request, file, callback) => {
+          const fileHash = crypto.randomBytes(16).toString('hex')
+          const filename = `${fileHash}-${file.originalname}`
+          callback(null, filename)
         }
+      })
     }
+  }
 }
