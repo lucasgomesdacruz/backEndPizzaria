d0b1d5de482e17985464021fc5cc8123969dcb1f
Lucas Gomes <lucaass1997rp@gmail.com>
2025-07-29 14:12:49 -0300

    feat: adiciona integração ao cloudinary e validação de img

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,15 @@ import cors from 'cors'
 import path from 'path'
 
 import { router } from './routes'
+import fileUpload from 'express-fileupload';
+
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 } //enviar no maximo 50mb
+}))
 
 app.use(router);
 
